41fc4b9871f584cac7d510df198078ecbb2f538a
SakshiVerma-9 <sakshiverma141111@gmail.com>
2026-07-13 23:39:30 +0530

    Added JavaScript files

---
+++ 01_Basics/03_typeconversion.js
@@ -0,0 +1,52 @@
+//let score = "33"
+//let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+// "" => false
+// "Sakshi" => true
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//************OPerations************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+/*
+console.log(2*2);
+console.log(2-2);
+console.log(2+2);
+console.log(2**3);
+console.log(3/2);
+console.log(3%2);
+*/
+let str1 = "Hello"
+let str2 = "Sakshi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + "2" + 2);
