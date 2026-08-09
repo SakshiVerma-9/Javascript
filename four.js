bf0be00dba49be349cd68e9d1f7e4d5f8de17b8d
SakshiVerma-9 <sakshiverma.141111@gmail.com>
2026-08-09 18:55:09 +0530

    Add files via upload

---
+++ four.js
@@ -0,0 +1,10 @@
+// For each loop does not return any value
+
+// const coding = ["js", "ruby", "java", "python", "cpp" ]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+// console.log(values);
+
