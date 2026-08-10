13f807c38c562276fdd0c0fada48ba8674926683
SakshiVerma-9 <sakshiverma.141111@gmail.com>
2026-08-10 14:44:04 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,50 @@
+// if
+
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if( temperature === 40){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+// const score = 200
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// var is completely global
+
+//const balance = 1000
+
+//if(balance>500)console.log("test"), console.log("test2"); //implicit scope  //wrong method
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance <750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+    
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFomGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+}
+if(loggedInFomGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
+
+
+
+
+
