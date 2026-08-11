823bb252e05057017d0014a2d376b6413a33843c
SakshiVerma-9 <sakshiverma.141111@gmail.com>
2026-08-11 22:48:23 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,42 @@
+// for loop
+
+// for(let index = 0; index < 10; index++){
+//     const element =index;
+//     console.log(element);
+// }
+
+// for(let i = 0; i<= 10; i++){
+//     console.log(`Outer loop value: ${i}`);
+//     for(let j = 0; j<= 10; j++){
+//        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+
+//         console.log(i + '*' + j + ' = ' + i*j);
+//     }
+//}
+
+//break and continue
+
+// for(let index = 1; index <= 20; index++){
+//     if(index==5){
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
+//while loop
+
+// let index =  0
+// while(index<= 10){
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+
+// }
+
+// do while
+
+let score = 10
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10);
